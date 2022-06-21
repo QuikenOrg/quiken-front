99325a3dbc6d3fc690a0616af3c28a2c8e95e44f
julianallenderussek <julianallenderussek@gmail.com>
2022-06-21 17:11:00 -0500

    sign up hight adjusted

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -52,12 +52,12 @@ const Navbar = () => {
           <Link className="navbar-link" to="/fulfillment">FULFILLMENT</Link>
           <Link className="navbar-link" to="/rastreo">RASTREO</Link>
           <Link className="navbar-link" to="/contacto">CONTACTO</Link>
-          {/* <Link className="navbar-link" to="/signin">
+          <Link className="navbar-link" to="/signin">
             SIGN IN
           </Link>
           <Link to="/signup">
             <BtnRegistro/>
-          </Link> */}
+          </Link>
         </div>
         {/* Mobile Menu */}
         <Burger open={open} setOpen={setOpen}/>
